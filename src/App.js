7bdb28771b2efe80fb2ac294a0f372dd95f666d6
9musa <99musahaider@gmail.com>
2025-11-13 23:04:49 +0500

    Changed array structure to objects containing title/content and added click logging

--- src/App.js
+++ src/App.js
@@ -11,31 +11,43 @@ function App() {
   const handleQueryChange = (value) => {
     setQuery(value);
   }
-  const topics = ["Python", "Archery", "Polynomials"];
+  const [topics, setTopics] = useState([{ title: "Python", content: "Emp" }, { title: "Archery", content: "pt" }, { title: "Polynomials", content: "ty" }]);
+  const [selTopic, setSelTopic] = useState(null)
   //filtered array containing matching items, strips each ##
   const filteredTopics = topics.filter(topic => {
-    return topic.toLowerCase().includes(query.toLowerCase().trim());
+    return topic.title.toLowerCase().includes(query.toLowerCase().trim());
   })
   //checks if atleast one match is found
   const doesQueryMatch = useMemo(() => {
     if (!query.trim()) {
       return false
     }
-    return topics.some(topic => topic.toLowerCase().includes(query.toLowerCase()))
+    return topics.some(topic => topic.title.toLowerCase().includes(query.toLowerCase()))
   }, [query, topics])
+  //returns true if any array element satisfies the condition
+  const handleAddTopic = (newTopic) => {
+    setTopics(prevTopic => [...prevTopic, newTopic]);
+  }
+  const handleTopicClick = (topic) => {
+    console.log(`Topic clicked: ${topic.title}`);
+    setSelTopic(topics)
+  }
+  //TROUBLESHOOTING
+  console.log(topics);
+  topics.forEach((t, i) => console.log(i, typeof t, t));
   return (
     <div className="App">
       <h1 className='txt'>Recall</h1>
       <div id="main">
         <Bar query={query} onQueryChange={handleQueryChange} />
         {query.trim() !== '' && (!doesQueryMatch ? (
-          <Button />
+          <Button onAdd={() => handleAddTopic(query)}/>
         ) : null)}
       </div>
       <div id="topicBox">
         <ul id="topicList">
           {filteredTopics.map((topic, index) => (
-            <li className='topicItem' key={index}>{topic}</li>
+            <li className='topicItem' key={index} onClick={() => handleTopicClick(topic)}>{topic.title}</li>
           ))}
         </ul>
       </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 import gsap from "gsap/gsap-core";
-function Button() {
+function Button({onAdd}) {
     const btnRef = useRef(null);
-ProcessingInstruction()
     useGSAP(() => {
         gsap.from(btnRef.current, {
             width: 0,
@@ -12,7 +11,7 @@ ProcessingInstruction()
         })
     }, [])
 
-    return <button className="btn" id="addBtn" ref={btnRef}>Add</button>
+    return <button className="btn" id="addBtn" ref={btnRef} onClick={onAdd}>Add</button>
 }
 
 export default Button;
